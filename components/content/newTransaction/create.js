a2b392bf9bdc891f3b3cb59dcf0b304dcfc910f0
Maria Aurellia <mariaadannies26@gmail.com>
2023-07-29 09:10:13 +0700

    fix create new transaction page

--- components/content/newTransaction/create.js
+++ components/content/newTransaction/create.js
@@ -1,7 +1,7 @@
-import {Input, Card, Button, Divider, Text, Link, Spacer} from '@nextui-org/react';
+import { Input, Card, Button, Divider, Text, Link, Spacer } from '@nextui-org/react';
 import React from 'react';
-import {Box} from '../../styles/box';
-import {Flex} from '../../styles/flex';
+import { Box } from '../../styles/box';
+import { Flex } from '../../styles/flex';
 import Image from 'next/image';
 import Cookies from 'js-cookie';
 import Router from 'next/router';
@@ -9,31 +9,32 @@ import axios from "axios";
 
 export const Content = () => {
    const [senderAccountNo, setSenderAccountNo] = React.useState("");
-  const [receiverAccountNo, setReceiverAccountNo] = React.useState("");
-  const [amount, setAmount] = React.useState("");
+   const [receiverAccountNo, setReceiverAccountNo] = React.useState("");
+   const [amount, setAmount] = React.useState("");
+   const [pesan, setPesan] = React.useState("");
 
-  const newTransactionHandler = async () => {
-    // initialize formData
-    const body = {"senderAccountNo" : senderAccountNo, "receiverAccountNo" : receiverAccountNo, "amount" : amount};
-    const headers = { 
-       'Content-Type': 'application/json',
-    };
+   const newTransactionHandler = async () => {
+      // initialize formData
+      const body = { "senderAccountNo": senderAccountNo, "receiverAccountNo": receiverAccountNo, "amount": amount };
+      const headers = {
+         'Content-Type': 'application/json',
+      };
 
-    // send data to server
-    await axios.post('https://34.101.154.14:8175/hackathon/bankAccount/transaction/create', body, headers)
-    .then((response) => {
+      // send data to server
+      await axios.post('https://34.101.154.14:8175/hackathon/bankAccount/transaction/create', body, headers)
+         .then((response) => {
 
-        // set token on cookies
-        Cookies.set('token', response.data.token);
+            // set token on cookies
+            Cookies.set('token', response.data.token);
 
-        // redirect to dashboard
-        Router.push('/service/dashboard');
-    })
-    .catch((error) => {
-      setPesan("Register error, please try again");
-    })
-  };
-   
+            // redirect to dashboard
+            Router.push('/service/dashboard');
+         })
+         .catch((error) => {
+            setPesan("error creating new transaction, please try again");
+         })
+   };
+
    return (
       <>
          <Flex
@@ -52,66 +53,66 @@ export const Content = () => {
             }}
             justify={'center'}
          >
-          <Card css={{ mw: '420px', p: '20px' }} variant="flat" style={{background: 'white'}}>
-            <Text
-              size={24}
-              css={{
-                as: 'center',
-                mb: '20px',
-              }}
-            >
-              New{' '}
-              <Text b size={24} weight="bold">
-                  Transaction
-              </Text>
-            </Text>
-            <Input
-                type="digit"
-                id="senderAccountNo"
-                name="senderAccountNo"
-                onChange={(e) => setSenderAccountNo(e.target.value)}
-                clearable
-                fullWidth
-                underlined
-                color="primary"
-                size="lg"
-                placeholder="Sender Account No"
-            />
-            <Spacer y={1} />
-            <Input
-                type="digit"
-                id="receiverAccountNo"
-                name="receiverAccountNo"
-                onChange={(e) => setReceiverAccountNo(e.target.value)}
-                clearable
-                fullWidth
-                underlined
-                color="primary"
-                size="lg"
-                placeholder="Receiver Account No"
-            />
-            <Spacer y={1} />
-            <Input
-                type="digit"
-                id="amount"
-                name="amount"
-                onChange={(e) => setAmount(e.target.value)}
-                clearable
-                fullWidth
-                underlined
-                color="primary"
-                size="lg"
-                placeholder="Amount"
-            />
-            <Spacer y={1} />
-            <Button onClick={newTransactionHandler}>Create</Button>
-            <Spacer y={1} />
-            <Button color="warning" css={{
-                as: 'center',
-              }} as={Link} href="/">
-                Back to Home
-            </Button>
-          </Card>
+            <Card css={{ mw: '420px', p: '20px' }} variant="flat" style={{ background: 'white' }}>
+               <Text
+                  size={24}
+                  css={{
+                     as: 'center',
+                     mb: '20px',
+                  }}
+               >
+                  New{' '}
+                  <Text b size={24} weight="bold">
+                     Transaction
+                  </Text>
+               </Text>
+               <Input
+                  type="digit"
+                  id="senderAccountNo"
+                  name="senderAccountNo"
+                  onChange={(e) => setSenderAccountNo(e.target.value)}
+                  clearable
+                  fullWidth
+                  underlined
+                  color="primary"
+                  size="lg"
+                  placeholder="Sender Account No"
+               />
+               <Spacer y={1} />
+               <Input
+                  type="digit"
+                  id="receiverAccountNo"
+                  name="receiverAccountNo"
+                  onChange={(e) => setReceiverAccountNo(e.target.value)}
+                  clearable
+                  fullWidth
+                  underlined
+                  color="primary"
+                  size="lg"
+                  placeholder="Receiver Account No"
+               />
+               <Spacer y={1} />
+               <Input
+                  type="digit"
+                  id="amount"
+                  name="amount"
+                  onChange={(e) => setAmount(e.target.value)}
+                  clearable
+                  fullWidth
+                  underlined
+                  color="primary"
+                  size="lg"
+                  placeholder="Amount"
+               />
+               <Spacer y={1} />
+               <Button onClick={newTransactionHandler}>Create</Button>
+               <Spacer y={1} />
+               <Button color="warning" css={{
+                  as: 'center',
+               }} as={Link} href="/">
+                  Back to Home
+               </Button>
+            </Card>
             <Box
                css={{
                   '& img': {
@@ -120,11 +121,11 @@ export const Content = () => {
                   },
                }}
             >
-               <Image src="/transaction.svg" alt="transaction" width={100} height={100}/>
+               <Image src="/transaction.svg" alt="transaction" width={100} height={100} />
             </Box>
          </Flex>
          <Divider
-            css={{position: 'absolute', inset: '0p', left: '0', mt: '$10'}}
+            css={{ position: 'absolute', inset: '0p', left: '0', mt: '$10' }}
          />
       </>
    );
