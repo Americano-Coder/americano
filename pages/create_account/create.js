21afa4325100a87537ca8f64b11bbc39896ec130
Maria Aurellia <mariaadannies26@gmail.com>
2023-07-29 11:06:17 +0700

    fix routes

--- pages/create_account/create.js
+++ pages/create_account/create.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Layout} from '../../components/navbar/layout';
 import {Box} from '../../components/styles/box';
-import {Content} from '../../components/content/register/registerContent';
+import {Content} from '../../components/content/newAccount/create';
 
 
 export default function RegisterPage() {
